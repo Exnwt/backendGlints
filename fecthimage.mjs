73a246204f4f0ac02095da72a5217efe3ddabc48
stevenlie <steven2002lie@gmail.com>
2023-01-27 12:29:36 +0700

    fix fetch import

--- fecthimage.mjs
+++ fecthimage.mjs
@@ -1,4 +1,16 @@
+import express from 'express';
 import fetch from 'node-fetch' ;
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => console.log(`Listening on PORT:${PORT}`));
+app.use(cors({origin:true}));
+
+
+
 
 fetch('https://www.flickr.com/services/feeds/photos_public.gne?format=json')
   .then((response) => response.text())
